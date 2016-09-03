cdd3d0cba6ee2334924325101e16bacaeab65e8a
pookzzz <w.kuttiya@gmail.com>
2016-09-03 13:27:18 +0000

    user interests

--- user-interest.rb
+++ user-interest.rb
@@ -3,7 +3,39 @@ require "sinatra/reloader" if development?
 require "tilt/erubis"
 require "yaml"
 
-get "/" do
+before do
   @users = YAML.load_file("users.yaml")
+end
+
+helpers do
+  def capitalize(text)
+    text.capitalize if text
+  end
+  
+  def user_count
+    @users.keys.size
+  end
+  
+  def interests_count
+    @users.inject(0) do |sum, (emails, interests)|
+      sum += interests.size
+    end
+  end
+end
+
+get "/" do
   erb :home
+end
+
+get "/users" do
+  erb :users
+end
+
+get "/:name" do
+  @user_name = params[:name].to_sym
+  
+  @email = @users[@user_name][:email]
+  @interests = @users[@user_name][:interests]
+  
+  erb :profile
 end
